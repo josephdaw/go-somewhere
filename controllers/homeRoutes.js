846dd743df8e5b5050af1430885998839629ae55
josephdaw <josephdaw@icloud.com>
2021-12-07 21:13:30 +1030

    adding homeRoute get request

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,53 +1,32 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Location, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+    // Get all locations
+    const locationData = await Location.findAll();
+    console.log('locationData: ', locationData)
 
     // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
+    const locations = projectData.map((project) => project.get({ plain: true }));
+    console.log('location: ',locations)
+
+    //extract unique city names
+    const cities = locations.map((city)=> city.city_name)
+    console.log('cities: ',cities)
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-      projects, 
+      cities, 
       logged_in: req.session.logged_in 
     });
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
 
-router.get('/project/:id', async (req, res) => {
-  try {
-    const projectData = await Project.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    const project = projectData.get({ plain: true });
-
-    res.render('project', {
-      ...project,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
@@ -72,8 +51,7 @@ router.get('/profile', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
+    return res.redirect('/profile');
   }
 
   res.render('login');
