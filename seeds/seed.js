d748c6aac8b13440ebc6a4f57faaa749dc6902a6
Stefanie Buntz <stbuntz@gmail.com>
2021-12-09 20:28:36 +1030

    models for pull request

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Location, Review } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const locationData = require('./locationData.json');
+const reviewData = require('./reviewData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,12 +13,20 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
+  for (const location of locationData) {
+    await Location.create({
+      ...location,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
+
+  for (const review of reviewData) {
+    await review.create({
+      ...review,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
 
   process.exit(0);
 };
