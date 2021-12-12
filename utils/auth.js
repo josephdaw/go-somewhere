28ccde51900893939da037a8c669e9640c75446d
josephdaw <josephdaw@icloud.com>
2021-12-12 20:54:49 +1030

    adding comments

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,11 @@
 const withAuth = (req, res, next) => {
   // If the user is not logged in, redirect the request to the login route
   if (!req.session.logged_in) {
+    // store the url that the user was trying to access before auth request
+    req.session.returnTo = req.originalUrl; 
+    // redirect to the login page
     res.redirect('/login');
+
   } else {
     next();
   }
