e040bd680f6e18864a2b7c71d0faa468beb3a410
Stefanie Buntz <stbuntz@gmail.com>
2021-12-14 20:29:53 +1030

    css and pretty stuff

--- public/js/login.js
+++ public/js/login.js
@@ -14,10 +14,42 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      const res = await response.json()
-      // If successful, redirect the browser to the page the user initially tried to access
-      document.location.replace(res.redirect);
+      // If successful, redirect the browser to the dashboard page
+      document.location.replace('/dashboard');
     } else {
+      console.log('err', response)
+      alert(response.statusText);
+    }
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const firstName = document.querySelector('#first-name-signup').value.trim();
+  const lastName = document.querySelector('#last-name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  console.log(firstName, lastName, email, password);
+
+  if (firstName && lastName && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ 
+        first_name: firstName,
+        last_name: lastName,
+        email,
+        password 
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      // If successful, redirect the browser to the dashboard page
+      document.location.replace('/dashboard');
+    } else {
+      console.log('err', response)
       alert(response.statusText);
     }
   }
@@ -27,3 +59,6 @@ document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
 
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
